c5d5255e036bb2da9ae6456ac3f609899370aba0
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-04-01 22:29:23 +0000

    chore: added loading state from defalt page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <h1>Hello world.</h1>;
+  const router = useRouter();
+
+  const { isLoggedIn, logout } = useAuthStore();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    } else {
+      router.push("/dashboard");
+    }
+  }, [isLoggedIn]);
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center w-screen h-screen">
+      <Loader className="size-[30px] animate-spin" />
+      <p className="text-lg text-center animate-pulse">Loading</p>
+    </div>
+  );
 }
